b7f913e1747ce841b93276d7808a214ccaa2892b
0carloscastillo0 <carlos.castillo.d@usach.cl>
2022-11-27 14:43:19 -0300

    edicion del backend

--- backendUX/src/main/java/com/example/backendUX/controller/UsuarioController.java
+++ backendUX/src/main/java/com/example/backendUX/controller/UsuarioController.java
@@ -5,12 +5,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.backendUX.model.Cuenta;
 import com.example.backendUX.model.Usuario;
+import com.example.backendUX.service.CuentaService;
 import com.example.backendUX.service.UsuarioService;
 
 @RestController
@@ -18,8 +21,10 @@ import com.example.backendUX.service.UsuarioService;
 @RequestMapping("usuario")
 public class UsuarioController {
     private final UsuarioService usuarioService;
-    public UsuarioController(UsuarioService usuarioService) {
+    private final CuentaService cuentaService;
+    public UsuarioController(UsuarioService usuarioService, CuentaService cuentaService) {
         this.usuarioService = usuarioService;
+        this.cuentaService = cuentaService;
     }
 
 
@@ -41,6 +46,21 @@ public class UsuarioController {
     @DeleteMapping(value="/deleteUs")
     public void deleteUsuario(){
         usuarioService.deleteUser();
+    }
+
+    //Crear cuenta y guardarlo en usuario (mediante el id)
+    @PostMapping(value = "/nuevaCuenta/{id}")
+    public ResponseEntity<Cuenta> save(@RequestBody Cuenta cuenta, @PathVariable String id){
+        // Se guarda la cuenta en la coleccion de cuentas
+        Cuenta nueva_cuenta = cuentaService.save(cuenta);
+
+        // Se guarda la cuenta dentro del usuario seleccionado
+        Usuario usuarioSeleccionado = usuarioService.getUser();
+        usuarioSeleccionado.getCuentas().add(nueva_cuenta);
+        Usuario cuentaAgregada = usuarioService.save(usuarioSeleccionado);
+
+        new ResponseEntity<Usuario>(cuentaAgregada, HttpStatus.OK);
+        return new ResponseEntity<Cuenta>(nueva_cuenta, HttpStatus.OK);
     }
 
 }
